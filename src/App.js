be63c7b46bd833b368a092b09631ebbeb7be5a2a
jswan <jswannieman@gmail.com>
2023-08-18 20:29:38 +1200

    Update App Javascript Component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
 // Hooks can Not be placed in Blocks
-//The useState Hook can Not be used in Class Components
+// The useState Hook can Not be used in Class Components
+
 import React, { useState } from "react";
 import "./App.css";
 
+const amount2Initial = () => {
+  return 50;
+};
+
 function App() {
+  // amount is our current state and setAmount is the function that allow us to update the state
+
   const [amount, setAmount] = useState(0);
 
-  const handleDecrementation = () => {
+  // The following example will avoid useState to be rendered more than once
+
+  const [amount2, setAmount2] = useState(() => amount2Initial());
+
+  // The following example illustrate how to use useState with objects
+
+  const [state, setState] = useState({ amount3: 999, color: "red" });
+  const amount3 = state.amount3;
+  const color = state.color;
+
+  // We use prevCount instead of count below, because if we should repeat setAmount(count - 1) more
+  // than once it will only subtract 1 from count. By using prevCount the update will take place,
+  // no matter how many times we repeat setCount(prevCount => prevCount - 1)
+
+  const handleDecrementation1 = () => {
     setAmount((prevAmount) => prevAmount - 1);
+    setAmount2((prevAmount2) => prevAmount2 - 10);
   };
 
-  const handleIncrementation = () => {
+  const handleIncrementation1 = () => {
     setAmount((prevAmount) => prevAmount + 1);
+    setAmount2((prevAmount2) => prevAmount2 + 20);
+  };
+
+  // Important to know that when the hook is getting updated for objects, that the
+  // previous values through the spread operator should also be updated, seeing that
+  // it is not getting merge in the object automatically
+
+  const handleDecrementation2 = () => {
+    setState((prevState) => {
+      return { ...prevState, amount3: prevState.amount3 - 1 };
+    });
+  };
+
+  const handleIncrementation2 = () => {
+    setState((prevState) => {
+      return { ...prevState, amount3: prevState.amount3 + 1 };
+    });
   };
   return (
     <div className="App">
       <div className="amount">
-        <button id="button1" onClick={handleDecrementation}>
+        <pre>Amount 1 + Amount 2</pre>
+        <button id="button1" className="button" onClick={handleDecrementation1}>
           -
         </button>
         <p>{amount}</p>
-        <button id="button2" onClick={handleIncrementation}>
+        <button id="button2" className="button" onClick={handleIncrementation1}>
+          +
+        </button>
+        <p>{amount2}</p>
+      </div>
+      <div className="amount">
+        <pre>Amount 3</pre>
+        <button id="button3" className="button" onClick={handleDecrementation2}>
+          -
+        </button>
+        <p>{amount3}</p>
+        <button id="button4" className="button" onClick={handleIncrementation2}>
           +
         </button>
       </div>
