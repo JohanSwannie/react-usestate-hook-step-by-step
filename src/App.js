b152d61e3490c02e776440a643e7503156f659f3
jswan <jswannieman@gmail.com>
2023-08-18 19:12:19 +1200

    Update App Javascript Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+// Hooks can Not be placed in Blocks
+//The useState Hook can Not be used in Class Components
 import React, { useState } from "react";
 import "./App.css";
 
 function App() {
   const [amount, setAmount] = useState(0);
+
+  const handleDecrementation = () => {
+    setAmount((prevAmount) => prevAmount - 1);
+  };
+
+  const handleIncrementation = () => {
+    setAmount((prevAmount) => prevAmount + 1);
+  };
   return (
     <div className="App">
-      <button>-</button>
-      <p>{amount}</p>
-      <button>+</button>
+      <div className="amount">
+        <button id="button1" onClick={handleDecrementation}>
+          -
+        </button>
+        <p>{amount}</p>
+        <button id="button2" onClick={handleIncrementation}>
+          +
+        </button>
+      </div>
     </div>
   );
 }
